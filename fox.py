ec66af5f5d42a9e47ff9d0736e7f55732f57e4a0
kkvelichko <94394316+kkvelichko@users.noreply.github.com>
2021-11-15 19:11:06 +0100

    Add files via upload

---
+++ fox.py
@@ -0,0 +1,10 @@
+@bot.command()
+async def fox(ctx):
+    response = requests.get('https://some-random-api.ml/img/fox') # Get-запрос
+    json_data = json.loads(response.text) # Извлекаем JSON
+
+    embed = discord.Embed(color = 0xff9900, title = 'Random Fox') # Создание Embed'a
+    embed.set_image(url = json_data['link']) # Устанавливаем картинку Embed'a
+    await ctx.send(embed = embed) # Отправляем Embed
+    
+    
